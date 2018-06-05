e6f3bd790a63ad718c4da024d7c94767be157e9a
monotonican <mono@monotonican.com>
2018-06-05 21:28:20 +0800

    add controller tests and fixtures

--- test/controllers/api/gossip_controller_test.rb
+++ test/controllers/api/gossip_controller_test.rb
@@ -3,51 +3,27 @@ require "test_helper"
 describe Api::GossipController do
 
   test "gossip" do
-    create_data
+    p User.count
+    p Message.count
+    alice = users(:alice)
+    bob = users(:bob)
+    cat = users(:cat)
 
-    akeys = {:curve=>"ed25519", :public=>"To2Qx7zeHi8WEKRNixo9-inFVzZaYSrIlLtk4ek5oGY~ed25519", :prvkey=>"gF8sxtMKy4WCstSXT6uwzhck4pNsN6RbWy-iJivyJOg~ed25519", :id=>"@To2Qx7zeHi8WEKRNixo9-inFVzZaYSrIlLtk4ek5oGY~ed25519"}
-    bkeys = {:curve=>"ed25519", :public=>"Oyzfr49vwAWuJK0eQtd-jXXbqpM9zIWa95KBLOnbTd4~ed25519", :prvkey=>"D9vJCDk35-vRKknn17HQWj5akILuSiZsjlplzYED2Fk~ed25519", :id=>"@Oyzfr49vwAWuJK0eQtd-jXXbqpM9zIWa95KBLOnbTd4~ed25519"}
-    
-    post '/api/gossip/connect', params: { key: bkeys[:public], port: 5001, host: 'localhost' }
+    post '/api/gossip/connect', params: { key: bob.key, port: 5001, host: 'localhost' }
     puts response.body
     p Peer.last.state_change.to_f
 
     sleep 0.1
-    post '/api/gossip/ping', params: { key: bkeys[:public] }
+    post '/api/gossip/ping', params: { key: bob.key }
     puts response.body
     p Peer.last.state_change.to_f
 
 
-    post '/api/gossip/sync_vector_clocks', params: { key: bkeys[:public], clocks: { akeys[:id] => 0 }, latest: 0, remote_latest: 0 }
+    post '/api/gossip/sync_vector_clocks', params: { key: bob.key, clocks: { alice.key => 0 }, latest: 0, remote_latest: 0 }
     puts response.body
 
-    post '/api/gossip/request_message', params: { key: bkeys[:public], notes: { akeys[:id] => { from: 0, to: 3 } } }
+    post '/api/gossip/request_message', params: { key: bob.key, notes: { alice.key => { from: 0, to: 3 } } }
     puts response.body
-
-
-  end
-
-  def create_data
-    akeys = {:curve=>"ed25519", :public=>"To2Qx7zeHi8WEKRNixo9-inFVzZaYSrIlLtk4ek5oGY~ed25519", :prvkey=>"gF8sxtMKy4WCstSXT6uwzhck4pNsN6RbWy-iJivyJOg~ed25519", :id=>"@To2Qx7zeHi8WEKRNixo9-inFVzZaYSrIlLtk4ek5oGY~ed25519"}
-    bkeys = {:curve=>"ed25519", :public=>"Oyzfr49vwAWuJK0eQtd-jXXbqpM9zIWa95KBLOnbTd4~ed25519", :prvkey=>"D9vJCDk35-vRKknn17HQWj5akILuSiZsjlplzYED2Fk~ed25519", :id=>"@Oyzfr49vwAWuJK0eQtd-jXXbqpM9zIWa95KBLOnbTd4~ed25519"}
-
-    alice = User.find_or_create_by(key: akeys[:id], prvkey: akeys[:prvkey], pubkey: akeys[:public], name: "alice")
-    bob = User.find_or_create_by(key: bkeys[:id], prvkey: bkeys[:prvkey], pubkey: bkeys[:public], name: "bob")
-
-    aid = akeys[:id]
-    bid = bkeys[:id]
-
-    alice.reload
-    msg = publish_post(akeys, "hello1", "test", nil, alice.seq, alice.previous, get_time)
-    add_message(msg)
-
-    alice.reload
-    msg = publish_post(akeys, "hello2", "test", nil, alice.seq, alice.previous, get_time)
-    add_message(msg)
-
-    alice.reload
-    msg = publish_post(akeys, "hello3", "test", nil, alice.seq, alice.previous, get_time)
-    add_message(msg)
   end
 
 end
--- test/controllers/api/posts_controller_test.rb
+++ test/controllers/api/posts_controller_test.rb
@@ -3,8 +3,6 @@ require 'uri'
 
 class Api::PostsControllerTest < ActionDispatch::IntegrationTest
   test "post" do
-    create_data
-
     post '/api/get_home_feed'
     puts response.body
 
@@ -12,28 +10,5 @@ class Api::PostsControllerTest < ActionDispatch::IntegrationTest
 
     post '/api/get_message/', params: { key: msgkey }
     puts response.body
-  end
-
-  def create_data
-    akeys = {:curve=>"ed25519", :public=>"To2Qx7zeHi8WEKRNixo9-inFVzZaYSrIlLtk4ek5oGY~ed25519", :prvkey=>"gF8sxtMKy4WCstSXT6uwzhck4pNsN6RbWy-iJivyJOg~ed25519", :id=>"@To2Qx7zeHi8WEKRNixo9-inFVzZaYSrIlLtk4ek5oGY~ed25519"}
-    bkeys = {:curve=>"ed25519", :public=>"Oyzfr49vwAWuJK0eQtd-jXXbqpM9zIWa95KBLOnbTd4~ed25519", :prvkey=>"D9vJCDk35-vRKknn17HQWj5akILuSiZsjlplzYED2Fk~ed25519", :id=>"@Oyzfr49vwAWuJK0eQtd-jXXbqpM9zIWa95KBLOnbTd4~ed25519"}
-
-    alice = User.find_or_create_by(key: akeys[:id], prvkey: akeys[:prvkey], pubkey: akeys[:public], name: "alice")
-    bob = User.find_or_create_by(key: bkeys[:id], prvkey: bkeys[:prvkey], pubkey: bkeys[:public], name: "bob")
-
-    aid = akeys[:id]
-    bid = bkeys[:id]
-
-    alice.reload
-    msg = publish_post(akeys, "hello1", "test", nil, alice.seq, alice.previous, get_time)
-    add_message(msg)
-
-    alice.reload
-    msg = publish_post(akeys, "hello2", "test", nil, alice.seq, alice.previous, get_time)
-    add_message(msg)
-
-    alice.reload
-    msg = publish_post(akeys, "hello3", "test", nil, alice.seq, alice.previous, get_time)
-    add_message(msg)
   end
 end
